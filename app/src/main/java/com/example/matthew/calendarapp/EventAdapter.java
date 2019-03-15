65be66e812f33ad1dd6ae23411f267dc6fd8579a
EuniceT <eunice.trinh@gmail.com>
2019-03-14 19:45:21 -0700

    All files

---
+++ app/src/main/java/com/example/matthew/calendarapp/EventAdapter.java
@@ -0,0 +1,70 @@
+package com.example.matthew.calendarapp;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
+
+public class EventAdapter extends ArrayAdapter<Event> {
+
+    private Activity mContext;
+    private List<Event> EventList;
+
+    public EventAdapter(Activity context, List<Event> list)
+    {
+        super( context, R.layout.activity_view_event, list);
+        mContext = context;
+        EventList = list;
+    }
+
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View listItem = LayoutInflater.from(mContext).inflate(R.layout.activity_event_adapter,null,true);
+        System.out.println("THIS IS ADAPTER HERE " + EventList);
+
+        Event currentEvent = EventList.get(position);
+
+        TextView name = (TextView) listItem.findViewById(R.id.eventName);
+        name.setText(currentEvent.getTitle());
+
+        TextView date = (TextView) listItem.findViewById(R.id.eventDate);
+        date.setText(currentEvent.getDate());
+
+        TextView time = (TextView) listItem.findViewById(R.id.eventTime);
+        time.setText(currentEvent.getTime());
+
+        TextView descript = (TextView) listItem.findViewById(R.id.eventDescript);
+        descript.setText(currentEvent.getDescription());
+
+
+        return listItem;
+        /*
+        View listItem = convertView;
+
+        if(listItem == null)
+            listItem = LayoutInflater.from(mContext).inflate(R.layout.activity_event_adapter,parent,false);
+
+        Event currentEvent = EventList.get(position);
+
+        TextView name = (TextView) listItem.findViewById(R.id.editTextTitle);
+        name.setText(currentEvent.getTitle());
+
+        TextView time = (TextView) listItem.findViewById(R.id.editTextTime);
+        time.setText(currentEvent.getTime());
+
+        TextView descript = (TextView) listItem.findViewById(R.id.editTextDescription);
+        descript.setText(currentEvent.getDescription());
+
+        return listItem;
+        */
+    }
+
+}
---
+++ app/src/main/java/com/example/matthew/calendarapp/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.matthew.calendarapp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    CalendarView calendarView;
+    TextView Date;
+    Button AddButton;
+    Button SButton;
+    Button RemoveButton;
+    String the_date;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        Date = (TextView) findViewById(R.id.Date);
+        AddButton = (Button) findViewById(R.id.Addbutton);
+        SButton = (Button) findViewById(R.id.Sbutton);
+        RemoveButton = (Button) findViewById(R.id.Removebutton);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2)
+            {
+                the_date = (i1+1) + "/" + i2 + "/" + i;
+                Date.setText(the_date);
+
+            }
+        });
+
+    }
+
+    public void EventAdding( View view)
+    {
+        Intent intent = new Intent( this, AddingActivity.class);
+        intent.putExtra("USER DATE", the_date);
+        startActivity(intent);
+    }
+
+    public void EventRemoval( View view)
+    {
+        Intent intent = new Intent( this, RemoveEvent.class);
+        startActivity(intent);
+    }
+
+
+    public void EventView( View view)
+    {
+        Intent intent = new Intent( this, ViewEvent.class);
+        startActivity(intent);
+    }
+}
